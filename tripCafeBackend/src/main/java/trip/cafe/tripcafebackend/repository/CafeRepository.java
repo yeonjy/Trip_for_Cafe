e7bc90c0524b9f735f064d1c675c343c5bdd6997
yeonjy <yeonjypaper@gmail.com>
2023-02-11 05:27:35 +0900

    feat: 지역별 카페 구현

--- tripCafeBackend/src/main/java/trip/cafe/tripcafebackend/repository/CafeRepository.java
+++ tripCafeBackend/src/main/java/trip/cafe/tripcafebackend/repository/CafeRepository.java
@@ -4,14 +4,19 @@ package trip.cafe.tripcafebackend.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import trip.cafe.tripcafebackend.domain.Cafe;
+import trip.cafe.tripcafebackend.dto.CafeResponseDTO;
+
+import java.util.List;
+import java.util.Optional;
 
 
 @Repository
 public interface CafeRepository extends JpaRepository<Cafe, Long> {
 
-    //    List<Cafe> findByLocation(String location);
-    //@EntityGraph(attributePaths = {""})
-
+    /**
+     * 카페 location별 조회
+     */
+    List<Cafe> findByLocation(String location);
 
 
 
